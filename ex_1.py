a149baacb1c233f3619c46b66170fa7bc6190bb2
Master <ivanakhmetev@gmail.com>
2023-06-10 11:45:06 +0300

    first commit

---
+++ ex_1.py
@@ -0,0 +1,112 @@
+class Node:
+
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_prev(self, val):
+        node = self.head
+        while node.next is not None:
+            if node.next.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_prev_node(self, nod):
+        node = self.head
+        count = 0
+        while node != nod:
+            count +=1 
+            node = node.next
+        node = self.head
+        while count != 1:
+            count -=1
+            node = node.next
+        return node
+
+
+    def find_next(self, val):
+        node = self.find(val)
+        if node is not None:
+            return node.next
+        return None
+
+    def is_value(self, val):
+        if self.find(val) == None:
+            return False
+        return True        
+
+    def find_all(self, val):
+        node = self.head
+        nodes = []
+        while node is not None:
+            if node.value == val:
+                nodes.append(node)
+            node = node.next
+        return nodes
+
+    def delete(self, val, all=False):
+        if self.is_value(val):
+            node_prev = self.find_prev(val)
+            node_next = self.find_next(val)
+            if val == self.head.value:
+                self.head = node_next
+            elif val == self.tail.value:
+                node_prev.next = None
+            else:
+                node_prev.next = node_next
+        else:
+            return
+        if all:
+            self.delete(val, True)
+
+    def clean(self):
+        node = self.head
+        while node is not None:
+            node.value = None
+            node = node.next
+
+    def len(self):
+        list_len = 0
+        node = self.head
+        while node is not None:
+            list_len += 1
+            node = node.next
+        return list_len
+
+    def insert(self, afterNode, newNode):
+        if afterNode is None:
+            newNode.next = self.head
+            self.head = newNode
+
+        else:
+            tmp_node = afterNode.next
+            afterNode.next = newNode
+            newNode.next = tmp_node
---
+++ ex_1test.py
@@ -0,0 +1,91 @@
+from ex_1 import LinkedList, Node
+
+values = [13, 12, 55, 13, 128, 'str', 13]
+values_2 = [12, 55, 128, 'str', 11]
+s_list = LinkedList()
+
+for el in values:
+    n = Node(el)
+    s_list.add_in_tail(n)
+
+def test_find(value):
+    n = s_list.find(value)
+    if n is not None:
+        print(n.value)
+
+def test_find_prev(value):
+    n = s_list.find_prev(value)
+    if n is not None:
+        print(n.value)
+
+def test_find_next(value):
+    n = s_list.find_next(value)
+    if n is not None:
+        print(n.value)
+
+def test_delete(value):
+    n = s_list.delete(value)
+    s_list.print_all_nodes()
+
+def test_delete_all(value):
+    n = s_list.delete(value, True)
+    s_list.print_all_nodes() 
+
+def test_clean():
+    s_list.print_all_nodes()
+    s_list.clean()
+    s_list.print_all_nodes()
+
+def test_find_all():
+    for el in values:
+        finds = s_list.find_all(el)
+        for el in finds:
+            print(el.value)
+
+def test_val():
+    s_list.add_in_tail(Node(50))
+    s_list.add_in_tail(Node(50))
+    print('target', 50)
+    test_delete(None)
+    # s_list.print_all_nodes()
+    for el in reversed(values):
+        print('target', el)
+        test_delete(el)
+    print('target', 50)
+    test_delete_all(50)
+    print('target', 50)
+    test_delete(50)
+    print('target', None)
+    test_delete(None)
+    for el in reversed(values):
+        print('target', el)
+        test_delete(el)
+
+def test_len():
+    print(s_list.len())
+
+def test_insert():
+    a = Node('kk')
+    b = Node('ww')
+    c = Node('l')
+    d = Node('beg')
+    s_list.add_in_tail(a)
+    s_list.add_in_tail(b)
+    b = s_list.find_prev_node(b)
+    s_list.insert(a, c)
+    s_list.insert(None, d)
+    s_list.print_all_nodes()
+
+    # s_list.insert(s_list.find('aa'), Node(14))
+    # s_list.print_all_nodes()
+
+    
+
+test_insert()
+# test_val()
+# test_len()
+# test_find_all()
+# test_clean()
+# print(values.clear())
+# test_val()
+
