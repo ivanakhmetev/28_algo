93ed2bb3ea2f27f2aadfec5f3abc369e981f6216
Master <ivanakhmetev@gmail.com>
2023-06-12 20:29:40 +0300

    2 / 2

---
+++ ex_2.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+class LinkedList2:  
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+            item.prev = None
+            item.next = None
+        else:
+            self.tail.next = item
+            item.prev = self.tail
+        self.tail = item
+
+    def print_all_nodes(self):
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        rtn = []
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                rtn.append(node)
+            node = node.next
+        return rtn
+
+    def delete(self, val, all=False):
+        pass # здесь будет ваш код
+
+    def clean(self):
+        pass # здесь будет ваш код
+
+    def len(self):
+        return 0 # здесь будет ваш код
+
+    def insert(self, afterNode, newNode):
+        pass # здесь будет ваш код
+
+    def add_in_head(self, newNode):
+        pass # здесь будет ваш код   
