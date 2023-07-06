6e1a55552ed93d0df68b8a1f4247d9c6dbb0f4af
Ахметьев Иван Алексеевич <iahmetiev@npomis.ru>
2023-07-06 15:43:26 +0300

    try

--- ex_10.py
+++ ex_10.py
@@ -1,8 +1,8 @@
 class HashTable:
     def __init__(self, sz, stp):
-        self.size = sz
+        self.sz = sz
         self.step = stp
-        self.slots = [None] * self.size
+        self.slots = [None] * self.sz
 
     def hash_fun(self, value):
         h_sum = 0
@@ -12,13 +12,13 @@ class HashTable:
         for el in value:
             h_sum = (h_sum + (ord(el) - ord('a') + 1)*h_pow) % m
             h_pow = h_pow * p % m
-        return h_sum % self.size
+        return h_sum % self.sz
 
     def seek_slot(self, value):
         slot = self.hash_fun(value)
         start_slot = slot  
         while self.slots[slot] is not None:
-            slot = (slot + self.step) % self.size
+            slot = (slot + self.step) % self.sz
             if slot == start_slot:
                 return None
         return slot
@@ -36,7 +36,7 @@ class HashTable:
         while self.slots[slot] is not None:
             if self.slots[slot] == value:
                 return slot
-            slot = (slot + self.step) % self.size
+            slot = (slot + self.step) % self.sz
             if slot == start_slot:
                 return None
             
@@ -47,36 +47,51 @@ class PowerSet(HashTable):
         super().__init__(20011, 19)
 
     def size(self):
-        return sum([1 for el in self.slots if el is None])
+        return sum([1 for el in self.slots if el is not None])
 
     def seek_slot(self, value):
         slot = self.hash_fun(value)
         start_slot = slot  
         while self.slots[slot] is not None:
-            slot = (slot + self.step) % self.size
+            slot = (slot + self.step) % self.sz
             if slot == start_slot:
                 return slot
         return slot
 
     def put(self, value):
+        if self.get(value):
+            return
         self.slots[self.seek_slot(value)] = value
 
     def get(self, value):
-        return self.slots[self.seek_slot(value)]
-        # slot = self.hash_fun(value)
-        # start_slot = slot  
-        # return False
+        return bool(self.find(value))
 
     def remove(self, value):
-        # возвращает True если value удалено
-        # иначе False
+        slot = self.find(value)
+        if slot is not None:
+            self.slots[slot] = None
+            return True
         return False
+    
+    def get_all(self):
+        return [el for el in self.slots if el is not None]
 
     def intersection(self, set2):
-        # пересечение текущего множества и set2
-        return None 
+        new_set = PowerSet()
+        elems_1 = self.get_all()
+        elems_2 = set2.get_all()
+        for el in elems_1:
+            if set2.get(el):
+                new_set.put(el)
+        for el in elems_2:
+            if self.get(el):
+                new_set.put(el)
+        return new_set
 
     def union(self, set2):
+        new_set = self.__init__()
+
+
         # объединение текущего множества и set2
         return None
 
---
+++ ex_10_test.py
@@ -0,0 +1,43 @@
+from ex_10 import PowerSet, HashTable
+
+# h = HashTable(200, 15)
+# t = 5
+# # print(t.size)
+# print(h.size)
+
+p = PowerSet()
+# print(type(p))
+# p.size()
+# print(p.size())
+# for i in range(10):
+#     p.put(str(i))
+#     print(p.size())
+
+# # for i in range(11):
+# #     print(p.get(str(i)))
+
+# # for i in range(3, 7):
+# #     print(p.get(str(i)))
+# #     print(p.remove(str(i)))
+# #     print(p.get(str(i)))
+
+# # for i in range(20):
+# #     print(p.remove(str(i)))
+
+# for i in range(11):
+#     print(p.get(str(i)))
+# # print(p.get_all())
+p = PowerSet()
+p2 = PowerSet()
+
+for i in range(10):
+    p.put(str(i))
+print(p.get_all())
+
+for i in range(5, 15):
+    # print(i)
+    p2.put(str(i))
+print(p2.get_all())
+
+p3 = p.intersection(p2)
+print(p3.get_all())
