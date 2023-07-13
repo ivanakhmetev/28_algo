6a8a3bcad749ed1f7aae3909915c82d068ec3499
Ахметьев Иван Алексеевич <iahmetiev@npomis.ru>
2023-07-13 11:20:13 +0300

    bloom

--- ex_11.py
+++ ex_11.py
@@ -4,9 +4,9 @@ class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
-        self.bit_array = self._gen_array0(f_len)
+        # self.bit_array = self._gen_array0(f_len)
         self.bits = 9223372036854775807
-        a = list(map(self.clear_bit, range(32)))
+        list(map(self.clear_bit, range(self.filter_len)))
 
     def hash1(self, str1):
         return reduce(self._hash1, str1, 0)
@@ -14,8 +14,8 @@ class BloomFilter:
     def _hash1(self, x, y):
         return (x * 17 + ord(y)) % self.filter_len
 
-    def _gen_array0(self, len):
-        return [bin(0)] * len
+    # def _gen_array0(self, len):
+    #     return [bin(0)] * len
 
     def hash2(self, str1):
         return reduce(self._hash2, str1, 0)
@@ -23,20 +23,19 @@ class BloomFilter:
     def _hash2(self, x, y):
         return (x * 223 + ord(y)) % self.filter_len
 
-    def _replace_bit(self, pos):
-        self.bit_array[pos] = bin(1)
+    # def _replace_bit(self, pos):
+    #     self.bit_array[pos] = bin(1)
 
     def add(self, str1):
-        self._replace_bit(self.hash1(str1))
-        self._replace_bit(self.hash2(str1))
+        # self._replace_bit(self.hash1(str1))
+        # self._replace_bit(self.hash2(str1))
         self.set_bit(self.hash1(str1))
         self.set_bit(self.hash2(str1))
 
-    def _is_value(self, str1):
-        print(self.hash1(str1), self.hash2(str1))
-        if self.bit_array[self.hash1(str1)] == bin(1) and self.bit_array[self.hash2(str1)] == bin(1):
-            return True
-        return False
+    # def _is_value(self, str1):
+    #     if self.bit_array[self.hash1(str1)] == bin(1) and self.bit_array[self.hash2(str1)] == bin(1):
+    #         return True
+    #     return False
     
     def is_value(self, str1):
         pos1 = self.hash1(str1)
@@ -53,7 +52,6 @@ class BloomFilter:
 
     def get_bit(self, pos):
         rep = self.bit_representation()
-        print(rep, pos)
         return rep[pos]
 
     def bit_representation(self):
--- ex_11_test.py
+++ ex_11_test.py
@@ -1,6 +1,6 @@
 from ex_11 import BloomFilter
 
-a = BloomFilter(64)
+a = BloomFilter(32)
 # print(a.bit_array)
 # print(int(a.bit_array[1], 2))
 # str1 = 'aaqwea'
@@ -17,7 +17,7 @@ a.add(string)
 for i in range(10):
     str1 = string[i:] + string[:i]
     # a.add(str1)
-    print(a.is_value(str1), a._is_value(str1))
+    print(a.is_value(str1))#, a._is_value(str1))
 
 
 # m = 3
