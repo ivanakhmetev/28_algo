0806c6b70e545d166685bd0a648f8ce244301817
Ахметьев Иван Алексеевич <iahmetiev@npomis.ru>
2023-07-13 11:14:59 +0300

    compare

--- ex_11.py
+++ ex_11.py
@@ -1,10 +1,12 @@
-from functools import reduce
+from functools import reduce#, map
 
 class BloomFilter:
 
     def __init__(self, f_len):
         self.filter_len = f_len
         self.bit_array = self._gen_array0(f_len)
+        self.bits = 9223372036854775807
+        a = list(map(self.clear_bit, range(32)))
 
     def hash1(self, str1):
         return reduce(self._hash1, str1, 0)
@@ -27,8 +29,38 @@ class BloomFilter:
     def add(self, str1):
         self._replace_bit(self.hash1(str1))
         self._replace_bit(self.hash2(str1))
+        self.set_bit(self.hash1(str1))
+        self.set_bit(self.hash2(str1))
 
-    def is_value(self, str1):
+    def _is_value(self, str1):
+        print(self.hash1(str1), self.hash2(str1))
         if self.bit_array[self.hash1(str1)] == bin(1) and self.bit_array[self.hash2(str1)] == bin(1):
             return True
         return False
+    
+    def is_value(self, str1):
+        pos1 = self.hash1(str1)
+        pos2 = self.hash2(str1)
+        if self.get_bit(pos1) == 1 and self.get_bit(pos2) == 1:
+            return True
+        return False
+    
+    def set_bit(self, bit):
+        self.bits = self.bits | (1 << bit)
+
+    def clear_bit(self, bit):
+        self.bits = self.bits & ~(1 << bit)
+
+    def get_bit(self, pos):
+        rep = self.bit_representation()
+        print(rep, pos)
+        return rep[pos]
+
+    def bit_representation(self):
+        rep = []
+        num = self.bits
+        while num != 0:
+            rep.append(num % 2)
+            num = num // 2
+        return rep
+
--- ex_11_test.py
+++ ex_11_test.py
@@ -1,6 +1,6 @@
 from ex_11 import BloomFilter
 
-a = BloomFilter(32)
+a = BloomFilter(64)
 # print(a.bit_array)
 # print(int(a.bit_array[1], 2))
 # str1 = 'aaqwea'
@@ -17,4 +17,19 @@ a.add(string)
 for i in range(10):
     str1 = string[i:] + string[:i]
     # a.add(str1)
-    print(a.is_value(str1))
+    print(a.is_value(str1), a._is_value(str1))
+
+
+# m = 3
+# import sys
+# print(sys.getsizeof(m))
+# a.set_bit(3)
+# print(a.bits)
+# print(a.bit_representation())
+# for i in range(31):
+#     a.clear_bit(i)
+#     # print(a.bits)
+#     print(a.bit_representation())
+# m = 2147483648
+# print(m.bit_length())
+
